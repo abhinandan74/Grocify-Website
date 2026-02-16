bcb90246862ab344d729e743ed94cc848731a6d2
Abhinandan Kumar <abhinandanp635@gmail.com>
2026-02-16 22:18:21 +0530

    Initial commit - Grocify website

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './component/Home/Home'
 
 const App = () => {
   return(
     <div>
-      <h1>Hello</h1>
+     <Home />
     </div>
   )
 }
---
+++ src/component/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Grocery from '../../assets/grocery.png' 
+
+const Hero = () => {
+  return (
+    <section>
+        <div className='max-w-[1400px] max-auto px-10px'>
+            {/* Hero Content */}
+            <div>
+
+            </div>
+            {/* Hero Image */}
+            <div>
+                <img src= {Grocery} alt="Hero Image" />
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Hero
