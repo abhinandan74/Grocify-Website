f7ded478817c9f390afa5e5d9d1d53d441fcc958
Abhinandan Kumar <abhinandanp635@gmail.com>
2026-02-25 23:23:34 +0530

    Initial grocery website frontend

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Hero from '../Hero/Hero'
+import Category from '../Category/Category'
+import Values from '../Values/Values'
+import Products from '../Products/Products'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Hero />
+      <Category />
+      <Values />
+      <Products />
     </div>
   )
 }
